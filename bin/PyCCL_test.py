ac797a3d4d12da504d4e91d4528cda874320bfee
davidesciotti <davidesciotti@gmail.com>
2022-05-02 19:31:47 +0200

    added KiDS-1000 halomodel recipe

--- bin/PyCCL_test.py
+++ bin/PyCCL_test.py
@@ -36,10 +36,9 @@ markersize = 10
 ###############################################################################
 
 # define fsky Euclid
-survey_area = 15000 # deg^2
-deg2_in_sphere = 41252.96 # deg^2 in a spere
-fsky_IST = survey_area/deg2_in_sphere
-
+survey_area = 15000  # deg^2
+deg2_in_sphere = 41252.96  # deg^2 in a spere
+fsky_IST = survey_area / deg2_in_sphere
 
 # Create new Cosmology object with a given set of parameters. This keeps track of previously-computed cosmological
 # functions
@@ -55,7 +54,11 @@ zi = np.array([
     [zmin, 0.418, 0.56, 0.678, 0.789, 0.9, 1.019, 1.155, 1.324, 1.576],
     [0.418, 0.56, 0.678, 0.789, 0.9, 1.019, 1.155, 1.324, 1.576, zmax]
 ])
+
 zbins = len(zi[0])
+
+# get number of redshift pairs
+npairs_auto, npairs_asimm, npairs_tot = mm.get_pairs(zbins)
 
 nzEuclid = 30 * (ztab / 0.9 * np.sqrt(2)) ** 2 * np.exp(-(ztab / 0.9 * np.sqrt(2)) ** 1.5)
 
@@ -64,8 +67,11 @@ fout, cb, zb, sigmab, c0, z0, sigma0 = 0.1, 1, 0, 0.05, 1, 0.1, 0.05
 nziEuclid = np.array([nzEuclid * 1 / 2 / c0 / cb * (cb * fout * (
         erf((ztab - z0 - c0 * zi[0, iz]) / np.sqrt(2) / (1 + ztab) / sigma0)
         - erf((ztab - z0 - c0 * zi[1, iz]) / np.sqrt(2) / (1 + ztab) / sigma0))
-        + c0 * (1 - fout) * (erf((ztab - zb - cb * zi[0, iz]) / np.sqrt(2) / (1 + ztab) / sigmab)
-        - erf((ztab - zb - cb * zi[1, iz]) / np.sqrt(2) / (1 + ztab) / sigmab))) for iz in range(zbins)])
+                                                    + c0 * (1 - fout) * (
+                                                                erf((ztab - zb - cb * zi[0, iz]) / np.sqrt(2) / (
+                                                                            1 + ztab) / sigmab)
+                                                                - erf((ztab - zb - cb * zi[1, iz]) / np.sqrt(2) / (
+                                                                    1 + ztab) / sigmab))) for iz in range(zbins)])
 
 # plt.xlabel('$z$')
 # plt.ylabel('$n_i(z)\,[\mathrm{arcmin}^{-2}]$')
@@ -95,12 +101,14 @@ PkFILE = np.genfromtxt(project_path / 'input/pkz-Fiducial.txt')
 
 # Populates vectors for z, k [1/Mpc], and P(k,z) [Mpc^3]
 zlist = np.unique(PkFILE[:, 0])
-klist = PkFILE[:int(len(PkFILE[:, 2]) / len(zlist)), 1] * cosmo.cosmo.params.h
-Pklist = PkFILE[:, 3].reshape(len(zlist), len(klist)) / cosmo.cosmo.params.h ** 3
+k_points = int(len(PkFILE[:, 2]) / len(zlist))
+klist = PkFILE[:k_points, 1] * cosmo.cosmo.params.h
+z_points = len(zlist)
+Pklist = PkFILE[:, 3].reshape(z_points, k_points) / cosmo.cosmo.params.h ** 3
 
 # Create a Pk2D object
 a_arr = 1 / (1 + zlist[::-1])
-lk_arr = np.log(klist)
+lk_arr = np.log(klist)  # it's the natural log, not log10
 Pk = ccl.Pk2D(a_arr=a_arr, lk_arr=lk_arr, pk_arr=Pklist, is_logp=False)
 
 # IST:F?
@@ -123,102 +131,104 @@ sigma_e = 0.3
 # notebook per mass_relations: https://github.com/LSSTDESC/CCLX/blob/master/Halo-mass-function-example.ipynb
 # Cl notebook: https://github.com/LSSTDESC/CCL/blob/v2.0.1/examples/3x2demo.ipynb
 
-# TODO we have no clue about the values of Delta and rho_type
-# name = 'Bhattacharya13'
+# TODO we're nto sure' about the values of Delta and rho_type
 # mass_def = ccl.halos.massdef.MassDef(Delta='vir', rho_type='matter', c_m_relation=name)
 
 # from https://ccl.readthedocs.io/en/latest/api/pyccl.halos.massdef.html?highlight=.halos.massdef.MassDef#pyccl.halos.massdef.MassDef200c
 
+# ! HALO MODEL PRESCRIPTIONS:
+# KiDS_1000 Methodology:
+# https://www.pure.ed.ac.uk/ws/portalfiles/portal/188893969/2007.01844v2.pdf, after (E.10)
 
+# Krause_2017: https://arxiv.org/pdf/1601.05779.pdf
+# about the mass definition, the paper says:
+# "Throughout this paper we define halo properties using the over density ∆ = 200 ¯ρ, with ¯ρ the mean matter density"
 
-
-# mass_def = ccl.halos.massdef.MassDef200m(c_m='Duffy08')
+hm_recipe = 'KiDS_1000'
 
 # ! mass definition
-# TODO check if they are the same
-# mass_def = ccl.halos.massdef.MassDef200m(c_m='Bhattacharya13')
-mass_def = ccl.halos.MassDef200m(c_m='Bhattacharya13')  # TODO is it really Bhattacharya13?
+if hm_recipe == 'KiDS_1000':
+    c_m = 'Duffy08'  # ! NOT SURE AT ALL!
+elif hm_recipe == 'Krause_2017':
+    c_m = 'Bhattacharya13'  # see paper, after Eq. 1
+
+mass_def = ccl.halos.MassDef200m(c_m=c_m)
 
 # TODO pass mass_def object? plus, understand what exactly is mass_def_strict
 # mass_def must not be None
-# don't use a default MassFunction or HaloBias classes, they're probably abstract classes
-# it's all default settings except for mass_def
 
 # ! mass function
-massfunc = ccl.halos.hmfunc.MassFuncTinker08(cosmo, mass_def=mass_def, mass_def_strict=True)
+massfunc = ccl.halos.hmfunc.MassFuncTinker10(cosmo, mass_def=mass_def, mass_def_strict=True)
 
 # ! halo bias
 hbias = ccl.halos.hbias.HaloBiasTinker10(cosmo, mass_def=mass_def, mass_def_strict=True)
 
 # ! concentration-mass relation
-c_M_relation = ccl.halos.concentration.ConcentrationBhattacharya13(mdef=mass_def)
+if hm_recipe == 'KiDS_1000':
+    c_M_relation = ccl.halos.concentration.ConcentrationDuffy08(mdef=mass_def)
+elif hm_recipe == 'Krause_2017':
+    c_M_relation = ccl.halos.concentration.ConcentrationBhattacharya13(mdef=mass_def)  # over Eq. 12
+
 
 # TODO understand better this object. We're calling the abstract class, is this ok?
+# ! HMCalculator
 hmc = ccl.halos.halo_model.HMCalculator(cosmo, massfunc, hbias, mass_def=mass_def,
                                         log10M_min=8.0, log10M_max=16.0, nlog10M=128,
                                         integration_method_M='simpson', k_min=1e-05)
 
-# old, delete
-# prof_GNFW = ccl.halos.profiles.HaloProfilePressureGNFW(mass_bias=0.8, P0=6.41, c500=1.81, alpha=1.33, alpha_P=0.12,
-#                                                        beta=4.13, gamma=0.31, P0_hexp=-1.0, qrange=(0.001, 1000.0),
-#                                                        nq=128, x_out=np.inf)
-
 # ! halo profile
 halo_profile = ccl.halos.profiles.HaloProfileNFW(c_M_relation=c_M_relation,
-                                    fourier_analytic=True, projected_analytic=False,
-                                    cumul2d_analytic=False, truncated=True)
+                                                 fourier_analytic=True, projected_analytic=False,
+                                                 cumul2d_analytic=False, truncated=True)
 
-
-# from https://ccl.readthedocs.io/en/latest/api/pyccl.halos.halo_model.html?highlight=trispectrum#pyccl.halos.halo_model.halomod_Tk3D_SSC
+# it was p_of_k_a=Pk, but it should use the LINEAR power spectrum (see documentation:
+# https://ccl.readthedocs.io/en/latest/api/pyccl.halos.halo_model.html?highlight=halomod_Tk3D_SSC#pyccl.halos.halo_model.halomod_Tk3D_SSC)
+# 🐛 bug solved: normprof shoud be True
 tkka = ccl.halos.halo_model.halomod_Tk3D_SSC(cosmo, hmc,
                                              prof1=halo_profile, prof2=None, prof12_2pt=None,
                                              prof3=None, prof4=None, prof34_2pt=None,
                                              normprof1=True, normprof2=True, normprof3=True, normprof4=True,
-                                             p_of_k_a=Pk, lk_arr=lk_arr, a_arr=a_arr, extrap_order_lok=1,
+                                             p_of_k_a=None, lk_arr=lk_arr, a_arr=a_arr, extrap_order_lok=1,
                                              extrap_order_hik=1, use_log=False)
 
-# Nbins = 10
+
+zbins = 2
 cov_PyCCL_6D = np.zeros((nbl, nbl, zbins, zbins, zbins, zbins))
-# TODO correct fsky?
 # for ell2_idx, ell2 in enumerate(ell): # TODO is this necessary?
 for i in range(zbins):
     for j in range(zbins):
         start = time.perf_counter()
         for k in range(zbins):
             for l in range(zbins):
-                # cltracer1, cltracer2 = CLL, CLL
-                # CLL_ij = ccl.angular_cl(cosmo, WL[i], WL[j], ell, p_of_k_a=Pk)
-                # CLL_kl = ccl.angular_cl(cosmo, WL_k, WL_l, ell, p_of_k_a=Pk)
-
                 cov_PyCCL_6D[:, :, i, j, k, l] = ccl.covariances.angular_cl_cov_SSC(cosmo, WL[i], WL[j], ell, tkka,
                                                                                     sigma2_B=None, fsky=fsky_IST,
                                                                                     cltracer3=WL[k], cltracer4=WL[l],
-                                                                                    ell2=None, integration_method='spline')
+                                                                                    ell2=None,
+                                                                                    integration_method='spline')
 
         print(f'i, j redshift bins: {i}, {j}, computed in  {(time.perf_counter() - start):.2f} seconds')
 
-
+mm.matshow(cov_PyCCL_6D[:, :, 0, 0, 0, 0], log=True, title='cov_PyCCL_6D')
 
 # SAVE
-# np.save(project_path / 'output/cov_SSC_stefanos_fix.npy', cov_SSC)
+# np.save(project_path / 'output/cov_SSC_p_of_k_a=None.npy', cov_SSC)
 
 
 ind = np.genfromtxt(
     '/Users/davide/Documents/Lavoro/Programmi/SSC_restructured/config/common_data/ind/indici_cloe_like.dat').astype(
     'int') - 1
-ind_LL = ind[:55, :]
+ind_LL = ind[:npairs_auto, :]
 
 # reshape
-cov_PyCCL_4D = mm.cov_6D_to_4D(cov_PyCCL_6D, nbl=nbl, npairs=55, ind=ind_LL)
-
+cov_PyCCL_4D = mm.cov_6D_to_4D(cov_PyCCL_6D, nbl=nbl, npairs=npairs_auto, ind=ind_LL)
 
 # import Robin and PySSC
-cov_Robin_2D = np.load('/Users/davide/Documents/Lavoro/Programmi/SSC_paper_jan22/PySSC_vs_CosmoLike/Robin/cov_SS_full_sky_rescaled/lmax5000_noextrap/davides_reshape/cov_R_WL_SSC_lmax5000_2D.npy')
+cov_Robin_2D = np.load(
+    '/Users/davide/Documents/Lavoro/Programmi/SSC_paper_jan22/PySSC_vs_CosmoLike/Robin/cov_SS_full_sky_rescaled/lmax5000_noextrap/davides_reshape/cov_R_WL_SSC_lmax5000_2D.npy')
 cov_PySSC_4D = np.load(project_path / 'input/CovMat-ShearShear-SSC-20bins-NL_flag_2_4D.npy')
 
-
 # reshape
-cov_Robin_4D = mm.cov_2D_to_4D(cov_Robin_2D, nbl=nbl, npairs=55, block_index='vincenzo')
+cov_Robin_4D = mm.cov_2D_to_4D(cov_Robin_2D, nbl=nbl, npairs=npairs_auto, block_index='vincenzo')
 cov_PySSC_6D = mm.cov_4D_to_6D(cov_PySSC_4D, nbl, zbins, probe='LL', ind=ind_LL)
 
 # show the various versions
@@ -238,7 +248,6 @@ mm.matshow(rob_over_PySSC[:, :, 0, 0], log=False, title='rob_over_PySSC')
 # corr_PySSC = mm.correlation_from_covariance(cov_SS_WL_old_6D[1, :, :, 0, 0, 0, 0])
 # mm.matshow(corr_PyCCL, log=True, title='PyCCL')
 # mm.matshow(corr_PySSC, log=True, title='PySSC')
-
 
 
 # from https://ccl.readthedocs.io/en/latest/api/pyccl.core.html?highlight=trispectrum#pyccl.core.Cosmology.angular_cl_cov_SSC
