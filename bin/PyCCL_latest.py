97a70422a6ce9231373e568f77c85c2c8619900a
Davide Sciotti <davidesciotti@gmail.com>
2023-05-26 19:43:53 +0200

    BIG changes to compute_derivatives, implemented the various relations among parameters

--- bin/PyCCL_latest.py
+++ bin/PyCCL_latest.py
@@ -142,6 +142,8 @@ def compute_3x2pt_PyCCL(ng_function, cosmo, kernel_dict, ell, tkka, f_sky, integ
                                                                             ind_AB=ind_dict[A + B],
                                                                             ind_CD=ind_dict[C + D],
                                                                             integration_method=integration_method)
+            return cov_ng_3x2pt_dict_10D
+
     if output_4D_array:
         return mm.cov_3x2pt_10D_to_4D(cov_ng_3x2pt_dict_10D, probe_ordering, nbl, zbins, ind.copy(), GL_or_LG)
 
@@ -323,40 +325,34 @@ for probe in probes:
             ind_CD = ind_dict[probe[0] + probe[1]]
 
             cov_ng_4D = compute_nongaussian_cov_ccl(cosmo_ccl,
-                                                    kernel_A=kernel_A, kernel_B=kernel_B, kernel_C=kernel_C,
-                                                    kernel_D=kernel_D,
+                                                    kernel_A=kernel_A, kernel_B=kernel_B,
+                                                    kernel_C=kernel_C, kernel_D=kernel_D,
                                                     ell=ell_grid, tkka=tkka, f_sky=f_sky, ng_function=ng_function,
                                                     integration_method=integration_method_dict[probe][which_NG],
                                                     ind_AB=ind_AB, ind_CD=ind_CD)
 
 
         elif probe == '3x2pt':
-            # let's test this:
+
             A, B, C, D = 'L', 'L', 'L', 'L'
-            cov_ng_3x2pt_dict_10D_test = {}
-            cov_ng_3x2pt_dict_10D_test[A, B, C, D] = compute_nongaussian_cov_ccl(cosmo_ccl,
-                                                                                 kernel_dict[A], kernel_dict[B],
-                                                                                 kernel_dict[C], kernel_dict[D],
-                                                                                 ell_grid, tkka, f_sky, ng_function,
-                                                                                 ind_AB=ind_dict[A + B],
-                                                                                 ind_CD=ind_dict[C + D],
-                                                                                 integration_method=
-                                                                                 integration_method_dict['LL'][
-                                                                                     which_NG])
-            cov_ng_4D_v2 = compute_nongaussian_cov_ccl(cosmo_ccl,
+            cov_ng_4D = compute_nongaussian_cov_ccl(cosmo_ccl,
                                                        kernel_A=kernel_dict[A], kernel_B=kernel_dict[B],
                                                        kernel_C=kernel_dict[C], kernel_D=kernel_dict[D],
                                                        ell=ell_grid, tkka=tkka, f_sky=f_sky, ng_function=ng_function,
                                                        integration_method=integration_method_dict[probe][which_NG],
                                                        ind_AB=ind_dict[A + B], ind_CD=ind_dict[C + D])
 
-            assert np.allclose(cov_ng_4D_v2, cov_ng_4D, rtol=1e-7, atol=0), 'cov_ng_4D_v2 != cov_ng_4D'
-
-            assert False, 'stop here'
+            cov_ng_2D = mm.cov_4D_to_2D(cov_ng_4D)
 
             cov_3x2pt_dict_10D = compute_3x2pt_PyCCL(ng_function, cosmo_ccl, kernel_dict, ell_grid, tkka, f_sky,
                                                      'qag_quad', probe_ordering, ind_dict,
                                                      output_4D_array=get_3xtpt_cov_in_4D)
+
+            cov_ng_3x2pt_dict_2D_test = mm.cov_4D_to_2D(cov_3x2pt_dict_10D['L', 'L', 'L', 'L'])
+            assert np.allclose(cov_ng_4D, cov_3x2pt_dict_10D['L', 'L', 'L', 'L'], rtol=1e-5,
+                               atol=0), 'cov_ng_4D_v2 != cov_ng_4D'
+            mm.compare_arrays(cov_ng_2D, cov_ng_3x2pt_dict_2D_test, plot_array=True, log_array=True, plot_diff=True)
+            assert False, 'stop here'
 
 
 
