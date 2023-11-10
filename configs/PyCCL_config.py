49423bce04f507c825b82fee6559ef6fbf44bfa9
Davide Sciotti <davidesciotti@gmail.com>
2023-11-10 17:15:01 +0100

    removed lib folder with symbolic links and common cfg files

---
+++ configs/PyCCL_config.py
@@ -0,0 +1,15 @@
+general_cfg = {
+    'zbins': 10,
+    'nbl': 30,
+    'triu_or_tril': 'triu',
+    'row_col': 'row',
+    'ell_recipe': 'ISTF',
+    'probes': ('3x2pt',),
+    'which_NGs': ('SSC', 'cNG'),
+    'save_covs': True,
+    'hm_recipe': 'Krause2017',
+    'GL_or_LG': 'GL',
+    'ell_min': 10,
+    'ell_max': 5000,
+    'use_ray': False,
+}
